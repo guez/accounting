88c1d03d13315af9124c416a6d757b86cd2eac58
Jorge <guez_95ii@hotmail.com>
2023-04-01 12:01:11 -0500

    [ADD] AccountingServiceProvider

---
+++ src/AccountingServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Guez\Accounting;
+
+use Illuminate\Support\ServiceProvider;
+
+class AccountingServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/Migrations');
+    }
+}
